d3d126eac58a1f6f431416d6f9d15fdbd3a4fba7
Lars Eric Kroll <32699019+lekroll@users.noreply.github.com>
2021-01-15 09:29:19 +0100

    add box component

--- Dashboard/src/app/components/infobox/box/box.component.ts
+++ Dashboard/src/app/components/infobox/box/box.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SelectControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-box',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./box.component.scss']
 })
 export class BoxComponent implements OnInit {
+@Input() value:number;
+@Input() title:string;
+@Input() description:string;
+@Input() colors:string[];
+@Input() cutoffs:string[];
+@Input() maincolor:string;
 
+  
   constructor() { }
+  backupcolor = "grey";
 
-  ngOnInit(): void {
-  }
+  
+  ngOnInit() {
+          this.setcolor();
+        };    
+    
+   setcolor(){
+     let color ="white";
+     if (this.maincolor){
+      color = this.maincolor;
+     }
+     if (this.cutoffs.length>0 && this.cutoffs.length == this.colors.length){
+
+     }
+     document.documentElement.style.setProperty('--bgcolor', color);
+   }
+    
 
 }
+
+
