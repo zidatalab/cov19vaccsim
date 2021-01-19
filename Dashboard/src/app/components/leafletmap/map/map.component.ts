3f6a3a2fb21650610a0805d58207778e3f71638e
Lars Eric Kroll <32699019+lekroll@users.noreply.github.com>
2021-01-19 13:28:08 +0100

    map is fixed

--- Dashboard/src/app/components/leafletmap/map/map.component.ts
+++ Dashboard/src/app/components/leafletmap/map/map.component.ts
@@ -1,10 +1,12 @@
 import { NgForOf, NgIf } from '@angular/common';
-import { AfterViewInit, Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AfterViewInit, Component, OnInit, Input, EventEmitter, Output, Renderer2 } from '@angular/core';
 import * as L from 'leaflet';
 import { icon, Marker } from 'leaflet';
 import { HttpClient } from '@angular/common/http';
 import { Feature, FeatureCollection, GeoJsonObject, GeoJsonTypes } from 'geojson';
 import { min, max, round, mean, std } from "mathjs";
+import * as chroma from "chroma-js";
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -13,48 +15,65 @@ import { min, max, round, mean, std } from "mathjs";
 })
 export class MapComponent implements OnInit {
   @Input() data: any;
-  @Input() maptitle: String;
   @Input() Outcome: String;
   @Input() Zoom: number;
   @Input() basemap: any;
   @Input() center: any;
   @Input() opacity: number;
-  globalmap: any;
   @Input() feature; string;
   @Input() colorscale: any;
   @Input() cutofflist: any;
   @Input() customlabels: any;
+  @Input() binmethod: string;
+  @Input() bins: number;
   @Input() id: string;
   legend: any;
   map: any;
-  constructor(private http: HttpClient) { }
+  containername : string;
+  constructor(private http: HttpClient, private renderer:Renderer2) { }
 
   ngOnInit(): void {
     // Init vars
-    if (!this.maptitle) { this.maptitle = ""; };
+    this.containername="mapdiv"+Math.round(Math.random()*1000).toString()+"_"+round(Math.random()*1000).toString();
     if (!this.Zoom) { this.Zoom = 5; };
     if (!this.center) { this.center = [51.948, 10.265]; };
     if (!this.opacity) { this.opacity = .6; };
     if (!this.customlabels) { this.customlabels = []; };
-    if (!this.colorscale) { this.colorscale = ['#800026', '#BD0026', '#E31A1C', '#FC4E2A', '#FD8D3C', '#FEB24C', '#FED976', '#FFEDA0']; };
-
+    console.log("Scale",this.makescale(10));
+    
+    
   }
   ngAfterViewInit(): void {
     // Import Map data
-    this.globalmap = L.map('map',
-      { center: this.center, zoom: this.Zoom }
-    );
-    this.initMap(this.globalmap);
-
+    this.initMap(this.containername);           
   }
 
   ngOnChanges(changes: any) {
-     this.initMap(this.globalmap);
+    // On any change remove map-container and add new map-container
+    if (changes.feature.previousValue){        
+      this.initMap(this.containername);           
+    };
+    
   }
 
+  preparedom(divid){
+    if(document.getElementById(divid))
+      {document.getElementById(divid).remove()};
+      let mapcontainer = this.renderer.createElement("div");
+      this.renderer.setProperty(mapcontainer, 'id', divid);
+      this.renderer.addClass(mapcontainer,"mapdiv");
+      this.renderer.appendChild(document.getElementById("map-frame"), mapcontainer);   
+    return true;
+  }
 
+  initMap(divid): void {
+    let mymap;
+    if (this.preparedom(divid)){
+      mymap = L.map(divid,
+        { center: this.center, zoom: this.Zoom }
+      );
+      }
 
-  initMap(map): void {
     // Fix Icons see https://stackoverflow.com/questions/41144319/leaflet-marker-not-found-production-env
     const iconRetinaUrl = 'assets/marker-icon-2x.png';
     const iconUrl = 'assets/marker-icon.png';
@@ -72,8 +91,6 @@ export class MapComponent implements OnInit {
     Marker.prototype.options.icon = iconDefault;
 
 
-    // Basemap
-    let mymap = map;
     
     // Openstreetmap Tiles
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -86,6 +103,12 @@ export class MapComponent implements OnInit {
     let geojsonFeature: FeatureCollection = this.basemap;
     let colors = this.colorscale;
     let cutoffs = this.cutofflist;
+    if (!cutoffs) {
+      cutoffs= this.makecutoffs(this.extractfeature(this.data,this.feature),this.binmethod,this.bins);              
+     };
+    if (colors.length<cutoffs.length){
+      colors = this.makescale(cutoffs.length)
+    }
     let propname = this.feature;
     let theid = this.id;
     let thedata = this.data;
@@ -120,11 +143,11 @@ export class MapComponent implements OnInit {
       return this._div;
     };
 
-    info.update = function (props, maptitle = this.maptitle) {
+    info.update = function (props) {
       this._div.innerHTML = (props ? props[theid] : "") + '<br>';
       let labelvalue = "";
-      // labelvalue = thefilter(thedata,theid,props[theid])[propname];
-      this._div.innerHTML += (props ? propname + "=" + '' + labelvalue : "");
+      // console.log("data:",thefilter(thedata, theid, props[theid]));
+      // this._div.innerHTML += (props ? propname + "=" + '' + labelvalue : "");
     };
 
     info.addTo(mymap);
@@ -153,7 +176,7 @@ export class MapComponent implements OnInit {
 
     legend.onAdd = function(map){
 
-      this._ldiv = L.DomUtil.create('legend', 'info legend');
+      this._ldiv = L.DomUtil.create('div', 'info legend');
       this._ldiv.innerHTML = '<p><strong>' + propname + '</strong></p>';
       for (var i = 0; i < cutoffs.length; i++) {
         if (labels.length == cutoffs.length) {
@@ -162,9 +185,16 @@ export class MapComponent implements OnInit {
             labels[i];
         }
         else {
+          if (colors.length<=5){
           this._ldiv.innerHTML +=
             '<i style="background-color:' + colors[i] + ';">&nbsp;&nbsp;&nbsp;</i> ' +
             cutoffs[i] + (cutoffs[i + 1] ? ' bis unter ' + cutoffs[i + 1] + '<br>' : '+');
+          }
+          else {
+            this._ldiv.innerHTML +=
+            '<i style="background-color:' + colors[i] + ';">&nbsp;&nbsp;&nbsp;</i> ' +
+            (cutoffs[i + 1] ? 'bis '+cutoffs[i + 1] +"&nbsp;" : cutoffs[i]+'+');
+          }
         }
       }
 
@@ -183,7 +213,7 @@ export class MapComponent implements OnInit {
       opacity: 1.0,
       fillOpacity: 1
     });
-    info.update(layer.feature.properties, this.maptitle);
+    info.update(layer.feature.properties);
   }
 
   resetFeature(info, e) {
@@ -195,21 +225,22 @@ export class MapComponent implements OnInit {
     });
     info.update();
   }
-  makecutoffs(array, method = "equalint", bins = 5) {
+  makecutoffs(array, method = "equalint", bins) {
     let result = [];
-    if (method == "equalint") {
-      let minv = min(array);
-      let maxv = max(array);
-      let steps = round(maxv - minv);
-      let i = 0;
+    let minv = min(array);
+    let maxv = max(array);
+    let steps = round((maxv - minv)/bins);
+    let i = 0;
       while (i < bins) {
         result.push((i + 1) * steps + minv);
+        i = i+1;
       }
-    }
     return result;
 
   }
 
+  
+  
   zoomToFeature(map, e) {
     map.fitBounds(e.target.getBounds());
   }
@@ -223,6 +254,39 @@ export class MapComponent implements OnInit {
     }
     return result;
   }
+
+  extractfeature(array,feature){
+    let result = [];
+    for (let item of array){
+      result.push(item[feature])
+    }
+    return result;
+  }
+
+  makescale(bins=5,startcolor="green",endcolor="red"){
+    let midcolor;
+    let colors = [startcolor,endcolor];
+    if (this.colorscale.length>2){
+      colors = [];
+      colors.push(this.colorscale[0]);           
+      colors.push(this.colorscale[1]);
+      colors.push(this.colorscale[2]);      
+    }
+    if (this.colorscale.length==2) {
+      colors=[];
+      colors.push(this.colorscale[0]);           
+      colors.push(this.colorscale[1]);            
+    }
+    let scale = chroma.scale(colors).mode('lab');
+    let i = 0;
+    let result=[];
+    while (i<bins){
+      result.push(scale((i+1)/bins).hex());
+      i = i+1;
+    }
+    return result;
+  }
+
 
 }
 
